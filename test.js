b0b8dd5119a810a0c6b2f0e3f3f900bb8c46c5b3
MO <fengzilong1992@gmail.com>
2018-10-23 23:36:02 +0800

    refactor: restructure files

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 const path = require( 'path' )
 const biu = require( './lib' )
+const fse = require( 'fs-extra' )
 
-const entry = path.resolve( __dirname, 'example/index.js' )
+const entry = path.resolve( __dirname, 'examples/simple/index.js' )
 const outDir = path.resolve( process.cwd(), '.out' )
 
-biu( entry, {
-  name: 'www',
-  outDir,
-  env: {
-    NODE_ENV: 'production'
-  }
-} )
+;( async () => {
+  await fse.emptyDir( outDir )
+
+  biu( entry, {
+    name: 'www',
+    outDir,
+    env: {
+      NODE_ENV: 'production'
+    }
+  } )
+} )()
